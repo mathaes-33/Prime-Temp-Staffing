a88190c38227e48d3c671d10cadbc923ff185c1e
mathaes33 <mathaes33@proton.me>
2025-07-30 20:42:14 -0400

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -72,3 +72,9 @@ export const InboxIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
 export const CalendarIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect><line x1="16" x2="16" y1="2" y2="6"></line><line x1="8" x2="8" y1="2" y2="6"></line><line x1="3" x2="21" y1="10" y2="10"></line></svg>
 );
+
+export const LoaderIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props} className={`animate-spin ${props.className}`}>
+        <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
+    </svg>
+);
